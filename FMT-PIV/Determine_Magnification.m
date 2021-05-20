07d7e470fd63b0cc54c382765ae4349fb2a9d400
Arthur Blomme <47719562+Bloempje@users.noreply.github.com>
2021-05-20 11:29:37 +0200

    Update Determine_Magnification.m

--- FMT-PIV/Determine_Magnification.m
+++ FMT-PIV/Determine_Magnification.m
@@ -2,9 +2,9 @@ clc; clear; close all;
 
 %% data
 % Folder where the calibration image is stored
-FoldRead = './data/Calibration/'; %(*** fill in ***)
+FoldRead = './data/Calibration/';
 FileRead = 'B00001.tif';
-pix_size =  4.40; % [microns] (*** fill in ***)
+pix_size =  4.40; % [microns]
 
 %% Determination of the magnification
 % read the image
@@ -13,14 +13,21 @@ im = imread([FoldRead FileRead]);
 % show the image using the imagesc function
 figure,clf, imagesc(im), axis equal, axis tight
 ttl = title('Select two reference points for calibration');
+
 % click on two points at known distance
 [x,y] =  ginput(2);
-% distance between two points in pixels (*** fill in ***)
-dist_px = 675;
+
+% distance between two points in pixels
+distx_px = x(2) - x(1);
+disty_px = y(2) - y(1);
+
 % input the distance in mm
-dist = input('Distance in mm? '); 
-% determine the magnification factor % (*** fill in ***)
-M = 0.0478; 
+dist = input('Distance in mm? ');
+
+% determine the magnification factor %
+M = (distx_px*pix_size)/(dist*10^3);
+% Reference: M = 0.0478;
+
 fprintf(['Magnification factor = ' num2str(M,'%.5f') '\n']);
 
 %% origin selection (in pixels)
