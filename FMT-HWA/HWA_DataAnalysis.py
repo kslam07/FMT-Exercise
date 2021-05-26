d402b3fb1082f9c7c42464b2a28712618d822951
HackermanThomas <t.j.broertjes@hotmail.com>
2021-05-26 14:35:23 +0200

    standard deviation hwa

--- FMT-HWA/HWA_DataAnalysis.py
+++ FMT-HWA/HWA_DataAnalysis.py
@@ -26,6 +26,7 @@ coef = np.polyfit(CalibrationData['Voltage'], CalibrationData['U'], 4)
 
 # plot polyfit
 voltages = np.linspace(1.1, 1.9, 100)
+plt.figure(4, constrained_layout=True, dpi=150)
 fig, ax = plt.subplots(1, 1, constrained_layout=True, dpi=150)
 ax.plot(voltages, np.polyval(coef, voltages), label=r"$4^{th}$-order polynomial fit", c='C00')
 ax.scatter(CalibrationData['Voltage'][::10000], CalibrationData['U'][::10000], label="measurements", c='C01', zorder=1)
@@ -33,6 +34,8 @@ ax.grid()
 ax.legend(prop={"size": 14})
 ax.set_xlabel("E [V]", fontsize=14)
 ax.set_ylabel("U [m/s]", fontsize=14)
+plt.savefig('HWA_Calibration.png', bbox_inches='tight')
+
 #################### 0 AOA ###############################
 ZeroAOAFolder = 'data/0 aoa'
 files = os.listdir(ZeroAOAFolder)
@@ -57,18 +60,37 @@ ZeroAoAData = ZeroAoAData[::2]
 # Create empty lists to append mean and rms
 ZeroAoA_Mean = []
 ZeroAoA_rms = []
+ZeroAoA_stdev = []
+
 for height in HeightList: # Loop over heights
     subset = ZeroAoAData.loc[ZeroAoAData['height']==height]     # filter specific height from df
     ZeroAoA_Mean.append(np.mean(subset['U']))                   # Calculate and append mean
     ZeroAoA_rms.append(np.sqrt(np.mean(subset['U']**2)))        # Calculate and append rms
+    subset['UminusMean2'] = (subset['U']-np.mean(subset['U']))**2  #
+    # subtract mean from entries
+    ZeroAoA_stdev.append(np.sqrt((1/(subset['UminusMean2'].size-1))*
+        subset[
+        'UminusMean2'].sum())) # compute standard deviation
+
+
+ZeroAoA_MeanPlus = np.array(ZeroAoA_Mean) + np.array(ZeroAoA_stdev) #Add
+# standard devioation
+ZeroAoA_MeanMinus = np.array(ZeroAoA_Mean) - np.array(ZeroAoA_stdev) #
+# Subtract standard deviation
+
 
 plt.figure(1, constrained_layout=True, dpi=150)
 plt.plot(ZeroAoA_Mean, HeightList, marker='D', label=r'$U_{mean}$')
-plt.plot(ZeroAoA_rms, HeightList, marker='X', linestyle='--', label=r'$U_{rms}$')
+#plt.plot(ZeroAoA_rms, HeightList, marker='X', linestyle='--', label=r'$U_{
+# rms}$')
+#plt.plot(ZeroAoA_stdev, HeightList, marker='^', label=r'StDev' )
+plt.plot(ZeroAoA_MeanPlus, HeightList, marker='^', label=r'Plus' ) #Plot plus
+plt.plot(ZeroAoA_MeanMinus, HeightList) # plot minus
 plt.xlabel('U [m/s]', fontsize=14)
 plt.ylabel('Height [mm]', fontsize=14)
 plt.title('Angle of Attack: 0$^\circ$', fontsize=14)
 plt.legend(loc=2)
+plt.grid()
 plt.savefig('HWA_0AoA.png', bbox_inches='tight')
 
 ########################### 5 AOA ################################3
@@ -100,12 +122,14 @@ for height in HeightList: # Loop over heights
     # append rms
 
 plt.figure(2)
-plt.plot(FiveAoA_Mean, HeightList, marker='D', label='U_{mean}')
-plt.plot(FiveAoA_rms, HeightList, marker='X', linestyle='--', label='U_{rms}')
+plt.plot(FiveAoA_Mean, HeightList, marker='D', label='$U_{mean}$')
+plt.plot(FiveAoA_rms, HeightList, marker='X', linestyle='--', label='$U_{'
+                                                                    'rms}$')
 plt.xlabel('U [m/s]', fontsize=14)
 plt.ylabel('Height [mm]', fontsize=14)
 plt.title('Angle of Attack: 5$^\circ$', fontsize=14)
 plt.legend(loc=2)
+plt.grid()
 plt.savefig('HWA_5AoA.png', bbox_inches='tight')
 
 
@@ -139,8 +163,9 @@ for height in HeightList: # Loop over heights
     # and append rms
 
 plt.figure(3)
-plt.plot(FifteenAoA_Mean, HeightList, marker='D', label='U_{mean}')
-plt.plot(FifteenAoA_rms, HeightList, marker='X', linestyle='--', label='U_{rms}')
+plt.plot(FifteenAoA_Mean, HeightList, marker='D', label='$U_{mean}$')
+plt.plot(FifteenAoA_rms, HeightList, marker='X', linestyle='--', label='$U_{'
+                                                                       'rms}$')
 plt.xlabel('U [m/s]', fontsize=14)
 plt.ylabel('Height [mm]', fontsize=14)
 plt.title('Angle of Attack: 15$^\circ$', fontsize=14)
